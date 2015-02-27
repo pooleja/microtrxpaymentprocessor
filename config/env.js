3e8f071613e37388e718249d3956079ff6820887
James Poole <poole.james@gmail.com>
2015-02-27 18:16:00 -0500

    added testnet config params

---
+++ config/env.js
@@ -0,0 +1,17 @@
+var bitcore = require('bitcore');
+
+// Configure the bitcoin network to use - this drives other config
+var bitcoinNetwork = bitcore.Networks.testnet;  // change to livenet if in production
+
+// Set default values for livenet
+var insightUrl = 'https://test-insight.bitpay.com';
+var connectionString = 'mongodb://localhost/testnet_microtrxgateway';
+
+// Override values if in testnet
+if(bitcoinNetwork == bitcore.Networks.testnet){
+  var insightUrl = 'https://insight.bitpay.com';
+  connectionString = 'mongodb://localhost/microtrxgateway';
+}
+
+exports.INSIGHT_URL = insightUrl;
+exports.MONGO_CONNECTION_STRING = connectionString;
