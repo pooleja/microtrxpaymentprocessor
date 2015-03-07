14c6b752b09b11f29f82dc1edeb73864947add9d
James Poole <poole.james@gmail.com>
2015-03-07 14:04:12 -0500

    fixed config issue

--- config/env.js
+++ config/env.js
@@ -4,13 +4,13 @@ var bitcore = require('bitcore');
 var bitcoinNetwork = bitcore.Networks.testnet;  // change to livenet if in production
 
 // Set default values for livenet
-var insightUrl = 'https://test-insight.bitpay.com';
-var connectionString = 'mongodb://localhost/testnet_microtrxgateway';
+var insightUrl = 'https://insight.bitpay.com';
+var connectionString = 'mongodb://localhost/microtrxgateway';
 
 // Override values if in testnet
 if(bitcoinNetwork == bitcore.Networks.testnet){
-  var insightUrl = 'https://insight.bitpay.com';
-  connectionString = 'mongodb://localhost/microtrxgateway';
+  var insightUrl = 'https://test-insight.bitpay.com';
+  connectionString = 'mongodb://localhost/testnet_microtrxgateway';
 }
 
 exports.INSIGHT_URL = insightUrl;
